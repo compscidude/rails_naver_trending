d7f70e2d433306e5381c0bded4b8c65559d83312
compscidude <grahammeuller@hotmail.com>
2017-04-13 14:48:51 +0900

    update validation rules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :users, through: :comments
     has_many :comments, dependent: :destroy
-    validates :name, uniqueness: {message: "Name already exists, please try a different name"}
+    validates :name, uniqueness: {message: "Name already exists, please try a different name"}, length: {minimum: 3}
 
     before_save { |user| user.name = user.name.downcase }
     # or before_save { self.name = self.name.downcase }
