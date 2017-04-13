2c77519dddc85af2716de9acd8c7b5906c0867c0
compscidude <grahammeuller@hotmail.com>
2017-04-13 14:34:58 +0900

    url changed to string format

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -3,12 +3,13 @@ module TopicsHelper
     def scrape  
         arr = []
         # add the ranks? or other href links later
-        doc = Nokogiri::HTML(open("http://www.naver.com/"))
+        doc = Nokogiri::HTML(File.read(open("http://www.naver.com/")))
         doc.css("li[data-order]").css(".ah_k").map do |a|
             arr << a.text
         end
         arr
      end
+
 
 
 end
